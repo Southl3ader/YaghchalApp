e57ac34ea988c3b92381275641c4158dd7e68d8a
Southl3ader <ip6217@yahoo.com>
2021-11-08 01:32:05 +0330

    Added bottommenu.xml

---
+++ app/src/main/java/com/example/yaghjava/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.yaghjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void toastMsg(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void displayToastMsg(View v) {
+        toastMsg("Hello how are you today!!");
+    }
+}
