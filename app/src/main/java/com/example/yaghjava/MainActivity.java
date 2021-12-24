61c7e6fa643dd949d6f455a74fe9b28fc93a5c97
rozh <rozhin.f78@gmail.com>
2021-12-25 00:49:39 +0330

    -All fruits and some vegetables were added to database

--- app/src/main/java/com/example/yaghjava/MainActivity.java
+++ app/src/main/java/com/example/yaghjava/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         addProtein();
         addCereal();
         addFruit();
+        addOthers();
         access.closeDB();
 
         new CountDownTimer(2000, 1000) {
@@ -66,11 +67,25 @@ public class MainActivity extends AppCompatActivity {
 
     //Add vegetables to database
     public void addVegetables(){
-        access.insert("گوجه", "Veg", imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.tomato)));
+        access.insert("گوجه فرنگی", "Veg", imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.tomato)));
         access.insert("هویج", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.carrot)));
         access.insert("کلم", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.cabbage)));
         access.insert("سیر", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.garlic)));
         access.insert("بادمجان", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.eggplant)));
+        access.insert("فلفل دلمه ای", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.bell_pepper)));
+        access.insert("فلفل", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.pepper)));
+        access.insert("چغندر", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.beetroot)));
+        access.insert("بروکلی", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.broccoli)));
+        access.insert("کدو حلوایی", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.pumpkin)));
+        access.insert("کرفس", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.celery)));
+        access.insert("تربچه", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.radish)));
+        access.insert("پیاز", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.onion)));
+        access.insert("قارچ", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.mushroom)));
+        access.insert("کاهو", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.lettuce)));
+        access.insert("سیب زمینی", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.potato)));
+        access.insert("خیار", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.cucumber)));
+        access.insert("نخود فرنگی", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.pea_pods)));
+        access.insert("زیتون", "Veg",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.olives)));
 
 
 
@@ -93,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
         access.insert("تخم مرغ", "Protein",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.egg)));
         access.insert("گوشت قرمز", "Protein",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.meat)));
         access.insert("ماهی", "Protein",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.fish)));
+        access.insert("مرغ", "Protein",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.chicken)));
 
 
 
@@ -101,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
     //Add Cereals to database
     public void addCereal(){
         access.insert("نان", "Cereal",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.bread)));
+        access.insert("ذرت", "Cereal",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.corn)));
 
 
 
@@ -113,6 +130,44 @@ public class MainActivity extends AppCompatActivity {
         access.insert("انگور", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.grape)));
         access.insert("توت فرنگی", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.strawberry)));
         access.insert("هلو", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.peach)));
+        access.insert("زردآلو", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.apricot)));
+        access.insert("آووکادو", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.avocado)));
+        access.insert("موز", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.banana)));
+        access.insert("توت سیاه", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.blackberry)));
+        access.insert("بلوبری", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.blueberry)));
+        access.insert("طالبی", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.cantaloupe)));
+        access.insert("گیلاس", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.cherry)));
+        access.insert("نارنگی", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.tangerine)));
+        access.insert("نارگیل", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.coconut)));
+        access.insert("انجیر", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.fig)));
+        access.insert("گریپ فروت", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.grapefruit)));
+        access.insert("خربزه", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.melon)));
+        access.insert("آلو سیاه", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.damson)));
+        access.insert("آلو زرد", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.mirabelle)));
+        access.insert("کیوی", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.kiwi)));
+        access.insert("لیمو", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.lemon)));
+        access.insert("لیمو شیرین", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.sweet_lemon)));
+        access.insert("انبه", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.mango)));
+        access.insert("توت", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.mulberry)));
+        access.insert("شلیل", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.nectarine)));
+        access.insert("پرتقال", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.orange)));
+        access.insert("گلابی", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.pear)));
+        access.insert("خرمالو", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.persimmon)));
+        access.insert("آناناس", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.pineapple)));
+        access.insert("انار", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.pomegranate)));
+        access.insert("به", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.quince)));
+        access.insert("تمشک", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.raspberry)));
+        access.insert("هندوانه", "Fruit",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.watermelon)));
+
+
+
+    }
+
+    public void addOthers(){
+        access.insert("سوسیس", "Others",imageToBytes(BitmapFactory.decodeResource(getResources(),R.drawable.sausage)));
+
+
+
 
     }
 
